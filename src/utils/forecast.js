51ab23f26f03553a3d575d9919f64f89ff4d4b2a
pr332y <35409036+pr332y@users.noreply.github.com>
2020-08-07 14:05:51 -0500

    Add new info to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast=(latitude,longitude,callback)=>{
         }else if(body.error){
             callback('Unable to detect location',undefined);
         }else{
-            callback(undefined,`${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees outside. It feels like ${body.current.feelslike} degrees.`);
+            callback(undefined,`${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees outside with humidity at ${body.current.humidity}%. It feels like ${body.current.feelslike} degrees.`);
             
         }
     })
